2423449f8aa9cc10e5ace1df5fd543cd39e8375e
FranticDreamer <edwardgmgmd@gmail.com>
2021-09-22 01:26:15 +0300

    Revert "Add Short Tool Tip to Int16"
    This reverts commit f9e88f2bf8a92cb965e65ac954454a5bffcd6dec.

--- Source/Int16forBPs/Public/Int16forBPsBPLibrary.h
+++ Source/Int16forBPs/Public/Int16forBPsBPLibrary.h
@@ -6,7 +6,7 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "Int16forBPsBPLibrary.generated.h"
 
-USTRUCT(BlueprintType, DisplayName = "Integer16", ShortToolTip = "16 Bit Integer Number")
+USTRUCT(BlueprintType, DisplayName = "Integer16")
 struct FInt16_bp
 {
 	GENERATED_BODY()
@@ -213,10 +213,10 @@ public:
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "ToFloorInt16 (float)", CompactNodeTitle = "->", BlueprintAutocast, Keywords = "toint16 cast convert"), Category = "Math|Int16")
 	static FInt16_bp Conv_FloatToFloorInt16_bp(float floatToConvert);
 
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Make Integer16", CompactNodeTitle = "»"), Category = "Math|Int16")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Make Integer16", CompactNodeTitle = "ï¿½"), Category = "Math|Int16")
 	static FInt16_bp Make16BitInt(uint8 A, uint8 B);
 
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Make Bytes", CompactNodeTitle = "»"), Category = "Math|Int16")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Make Bytes", CompactNodeTitle = "ï¿½"), Category = "Math|Int16")
 	static void Make8BitInts(FInt16_bp A, uint8& O1, uint8& O2);
 
 	#pragma endregion
